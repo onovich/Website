ed51b4596be01c0fa74fb5da2dcef9f2cc6186b4
onovich <hjn1110@163.com>
2024-05-16 05:40:49 +0800

    <feature> modify: link label 可配置

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const projects = [
-    { title: 'Ninja Ming', description: '开发中 (协助开发)', link: 'https://github.com/onovich/Swing', img: 'images/codes/codecover_compass.png', imgLink: 'https://example.com/ninja' },
+    { title: 'Ninja Ming', description: '开发中 (协助开发)', link: 'https://github.com/onovich/Swing', linkLabel: 'Github', img: 'images/codes/codecover_compass.png', imgLink: 'https://example.com/ninja' },
     { title: 'Oshi', description: '2024 研发中', img: 'images/codes/codecover_compass.png' },
     { title: 'Alter', description: '2024 研发中', img: 'images/codes/codecover_compass.png' },
     { title: 'Leap', description: '2024 研发中', img: 'images/codes/codecover_compass.png' },
     { title: 'Zangeki60s', description: '2024 研发中', img: 'images/codes/codecover_compass.png' },
-    { title: 'Ping', description: '2024.3', link: 'https://new-link.com/ping', img: 'images/codes/codecover_compass.png', imgLink: 'https://example.com/ping' },
-    { title: 'FrogVillage', description: '已推出 (合作开发)', link: 'https://new-link.com/frogvillage', img: 'images/codes/codecover_compass.png' },
-    { title: 'VillageTD', description: '已推出 (协助开发)', link: 'https://new-link.com/villagetd', img: 'images/codes/codecover_compass.png' },
+    { title: 'Ping', description: '2024.3', link: 'https://new-link.com/ping', linkLabel: 'Ping Project', img: 'images/codes/codecover_compass.png', imgLink: 'https://example.com/ping' },
+    { title: 'FrogVillage', description: '已推出 (合作开发)', link: 'https://new-link.com/frogvillage', linkLabel: 'Frog Village', img: 'images/codes/codecover_compass.png' },
+    { title: 'VillageTD', description: '已推出 (协助开发)', link: 'https://new-link.com/villagetd', linkLabel: 'Village TD', img: 'images/codes/codecover_compass.png' },
     { title: 'Narrow', description: '已推出', img: 'images/codes/codecover_compass.png' },
 ];
 
@@ -22,7 +22,7 @@ projects.forEach(project => {
         <img src="${project.img}" alt="${project.title}" ${imgLinkAttribute}>
         <h3><b>${project.title}</b></h3>
         <p>${project.description}</p>
-        ${project.link ? `<a href="${project.link}" target="_blank">Github</a>` : ''}
+        ${project.link ? `<a href="${project.link}" target="_blank">${project.linkLabel}</a>` : ''}
     `;
 
     // 点击图片时的处理
