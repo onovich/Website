62ce698ea25bc2e323da356a5bb78822640f05eb
onovich <hjn1110@163.com>
2024-05-24 05:04:48 +0800

    <feature> add: Game Page

--- sidebar.js
+++ sidebar.js
@@ -5,15 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="sidebar">
                 <h2><a href="index.html">Onovich</a></h2>
                 <a href="codes.html" class="active">CODES</a>
-                <a href="#">GAMES</a>
-                <a href="#">PIXEL ARTS</a>
-                <a href="#">ILLUSTRATIONS</a>
-                <a href="#">GIFS</a>
-                <a href="#">GRAPHIC DESIGNS</a>
-                <a href="#">PHOTOS</a>
-                <a href="#">POEMS</a>
-                <a href="#">SNS</a>
-                <a href="#">MESSAGE</a>
+                <a href="games.html">GAMES</a>
+                <a href="pixel-arts.html">PIXEL ARTS</a>
+                <a href="illustrations.html">ILLUSTRATIONS</a>
+                <a href="gifs.html">GIFS</a>
+                <a href="graphic-designs.html">GRAPHIC DESIGNS</a>
+                <a href="photos.html">PHOTOS</a>
+                <a href="poems.html">POEMS</a>
+                <a href="sns.html">SNS</a>
+                <a href="message.html">MESSAGE</a>
             </div>
         `;
         sidebarPlaceholder.innerHTML = sidebarContent;
